34c3aa74ab48b65363d89c9650919fe36f6e2eb4
zhixingj <69444736+zhixingj@users.noreply.github.com>
2020-09-11 10:29:51 -0500

    test

--- flaskr/static/hello.js
+++ flaskr/static/hello.js
@@ -1,5 +1,5 @@
 window.addEventListener('mousemove', e=>{
-    console.log("1");
+    console.log("2");
     document.getElementById('x-coord').textContent=e.x;
     document.getElementById('y-coord').textContent=e.y;
 })
