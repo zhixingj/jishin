ae50dd9e5c144269114eb506612e82cdc2c6423c
zhixingj <69444736+zhixingj@users.noreply.github.com>
2020-09-11 11:54:29 -0500

    background grid & corner dots

--- flaskr/static/hello.js
+++ flaskr/static/hello.js
@@ -4,18 +4,3 @@ window.addEventListener('mousemove', e=>{
     document.getElementById('y-coord').textContent=e.y;
 })
 
-window.onload = drawTimeLine;
-function drawTimeLine() {
-    var canva = document.getElementById('my_canvas');
-    if (canva.getContext('2d')) {
-        var ctx = canva.getContext('2d');
-        console.log(ctx);
-
-        ctx.beginPath();
-        ctx.moveTo(44,221);
-        ctx.lineTo(100,100);
-        ctx.stroke();
-
-    }
-}
-
