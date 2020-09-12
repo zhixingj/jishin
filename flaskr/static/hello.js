665aa67196038bfbb5b9fc572e025fa71b67b437
Xinzhe Chai <xinzhe_chai@brown.edu>
2020-09-11 21:44:31 -0500

    added timeline mouseover

--- flaskr/static/hello.js
+++ flaskr/static/hello.js
@@ -1,5 +1,24 @@
-window.addEventListener('mousemove', e=>{
-    console.log("1");
-    document.getElementById('x-coord').textContent=e.clientX;
-    document.getElementById('y-coord').textContent=e.clientY;
-})
+window.onload = init;
+
+function init() {
+    window.addEventListener('mousemove', e => {
+        document.getElementById('x-coord').textContent = e.pageX;
+        document.getElementById('y-coord').textContent = e.pageY;
+    })
+
+    $("#timeLine").mouseover(function () {
+        var mousetip_time = document.getElementById('mousetip-time');
+        mousetip_time.style.display = "block";
+        window.onmousemove = function (e) {
+            var x = e.pageX,
+                y = e.pageY;
+            var timeline_x = document.getElementById('timeLine').getBoundingClientRect().left;
+            mousetip_time.style.top = (y - 10) + 'px';
+            mousetip_time.style.left = (timeline_x - mousetip_time.clientWidth - 20) + 'px';
+        };
+    });
+    $("#timeLine").mouseout(function () {
+        var mousetip_time = document.getElementById('mousetip-time');
+        mousetip_time.style.display = "none";
+    });
+}
