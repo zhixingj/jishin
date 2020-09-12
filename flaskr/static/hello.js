1ac5a093a9707bc162f5eea8d1acdfdfd63978dd
zhixingj <69444736+zhixingj@users.noreply.github.com>
2020-09-12 13:31:52 -0500

    try svg

--- flaskr/static/hello.js
+++ flaskr/static/hello.js
@@ -9,6 +9,8 @@ var block = null;
 var coord = {x:0, y:0}; //initial mouse coord
 var paint = false;
 var startCorner = {x:0,y:0};
+const svg = d3.select('#svg');
+
 function getPosition(event) {
     coord.x = event.pageX+document.body.scrollLeft;
     coord.y = event.pageY+document.body.scrollTop;
@@ -16,14 +18,16 @@ function getPosition(event) {
 function startDrawing(event) {
     paint = true;
     getPosition(event);
+
+    let top = pxToVh(coord.y);
+    var rect = svg.append('rect').attr('x',pxToVw(coord.x)+'vw').attr('y', top+'vh');
+
     block = document.createElement('div');
     block.className = 'block';
-    let top = pxToVh(coord.y);
     block.style.top = top+'vh';
     startCorner.x = 24.6+'vw';
     startCorner.y = top;
     document.body.appendChild(block);
-    console.log("started");
 }
 
 function draw(event) {
@@ -33,15 +37,16 @@ function draw(event) {
     getPosition(event);
     let width = pxToVw(coord.x) - parseInt(startCorner.x);
     let height = pxToVh(coord.y) - parseInt(startCorner.y);
+
+    rect.attr('x', width+'vw').attr('y', height+'vh');
+    
     block.style.width=width+'vw';
     block.style.height=height+'vh';
     console.log("drawing");
-    
 }
 
 function stopDrawing(event){
     paint=false;
-    // currentBlock.style.width=20;
 
 }
 
