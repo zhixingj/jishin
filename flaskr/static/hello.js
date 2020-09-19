6e90ac8459fd3be7bd75ef8552dfb30740f146e8
Xinzhe Chai <xinzhe_chai@brown.edu>
2020-09-18 22:14:54 -0500

    added time label

--- flaskr/static/hello.js
+++ flaskr/static/hello.js
@@ -1,24 +1,11 @@
-window.addEventListener('load', event=>{
+window.onload = init;
+
+function init() {
     document.addEventListener('mousedown', startDrawing);
     document.addEventListener('mousemove', draw);
     document.addEventListener('mouseup', stopDrawing);
-
-    $("#timeLine").mouseover(function () {
-        var mousetip_time = document.getElementById('mousetip-time');
-        mousetip_time.style.display = "block";
-        window.onmousemove = function (e) {
-            var x = e.pageX,
-                y = e.pageY;
-            var timeline_x = document.getElementById('timeLine').getBoundingClientRect().left;
-            mousetip_time.style.top = (y - 10) + 'px';
-            mousetip_time.style.left = (timeline_x - mousetip_time.clientWidth - 20) + 'px';
-        };
-    });
-    $("#timeLine").mouseout(function () {
-        var mousetip_time = document.getElementById('mousetip-time');
-        mousetip_time.style.display = "none";
-    });
-})
+    $("#timeLine").mouseenter(timeLabelEnter).mouseout(timeLabelOut);
+};
 
 var block = null;
 var coord = {x:0, y:0}; //initial mouse coord
@@ -70,5 +57,21 @@ function pxToVh(px) {
     return px*(100/document.documentElement.clientHeight);
 }
 
+// chai's functions
+function timeLabelEnter() {
+    console.log("shit");
+    var mousetip_time = document.getElementById('mousetip-time');
+    mousetip_time.style.display = "block";
+    window.onmousemove = function (e) {
+        var x = e.pageX,
+            y = e.pageY;
+        var timeline_x = document.getElementById('timeLine').getBoundingClientRect().left;
+        mousetip_time.style.top = (y - 10) + 'px';
+        mousetip_time.style.left = (timeline_x - mousetip_time.clientWidth - 20) + 'px';
+    }
+}
 
-
+function timeLabelOut() {
+    var mousetip_time = document.getElementById('mousetip-time');
+        mousetip_time.style.display = "none";
+}
