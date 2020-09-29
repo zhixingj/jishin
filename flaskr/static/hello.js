a36382306677478ec0d34563b55126359485c32d
zhixingj <69444736+zhixingj@users.noreply.github.com>
2020-09-28 22:27:32 -0500

    svg block done

--- flaskr/static/hello.js
+++ flaskr/static/hello.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', event=>{
-    document.addEventListener('mousedown', startDrawing);
-    document.addEventListener('mousemove', draw);
-    document.addEventListener('mouseup', stopDrawing);
+    svg = d3.select('#svg');
+    svg.on('mousedown', startDrawing);
+    // document.addEventListener('mousemove', draw);
+    svg.on('mousemove', draw);
+    svg.on('mouseup', stopDrawing);
     
 })
 
@@ -9,40 +11,41 @@ var block = null;
 var coord = {x:0, y:0}; //initial mouse coord
 var paint = false;
 var startCorner = {x:0,y:0};
-const svg = d3.select('#svg');
+var rect = null;
 
 function getPosition(event) {
     coord.x = event.pageX+document.body.scrollLeft;
     coord.y = event.pageY+document.body.scrollTop;
 }
 function startDrawing(event) {
+    console.log('started drawing');
     paint = true;
-    getPosition(event);
+    // getPosition(event);
 
-    let top = pxToVh(coord.y);
-    var rect = svg.append('rect').attr('x',pxToVw(coord.x)+'vw').attr('y', top+'vh');
-
-    block = document.createElement('div');
-    block.className = 'block';
-    block.style.top = top+'vh';
-    startCorner.x = 24.6+'vw';
+    let top = pxToVh(d3.event.pageY);
+    console.log(`rect: ${rect}`);
+    //     block = document.createElement('div');
+    //     block.className = 'block';
+    //     block.style.top = top+'vh';
+    startCorner.x = 24+'vw';
     startCorner.y = top;
-    document.body.appendChild(block);
+    rect = svg.append('rect').attr('x',startCorner.x).attr('y', top+'vh').style('fill',"cornflowerblue");
+//     document.body.appendChild(block);
+// }
 }
-
 function draw(event) {
     if (!paint){
         return;
     }
-    getPosition(event);
-    let width = pxToVw(coord.x) - parseInt(startCorner.x);
-    let height = pxToVh(coord.y) - parseInt(startCorner.y);
 
-    rect.attr('x', width+'vw').attr('y', height+'vh');
+    // getPosition(event);
+    let width = pxToVw(d3.event.pageX) - parseInt(startCorner.x);
+    let height = pxToVh(d3.event.pageY) - parseInt(startCorner.y);
+
+    rect.attr('width', width+'vw').attr('height', height+'vh');
     
-    block.style.width=width+'vw';
-    block.style.height=height+'vh';
-    console.log("drawing");
+    // block.style.width=width+'vw';
+    // block.style.height=height+'vh';
 }
 
 function stopDrawing(event){
@@ -57,6 +60,3 @@ function pxToVw(px) {
 function pxToVh(px) {
     return px*(100/document.documentElement.clientHeight);
 }
-
-
-
